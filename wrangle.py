40d3f28662bd40b069c1695665653a0e69149c48
Mark Foster <markdouglasfoster@gmail.com>
2022-12-01 14:11:53 -0600

    mine

--- wrangle.py
+++ wrangle.py
@@ -147,15 +147,9 @@ def split_data(df, test_size=0.15):
 
 def scale_zillow(train, validate, test, target):
     '''
-<<<<<<< HEAD
     Takes in train, validate, test and the target variable.
     Returns df with new columns with scaled data for the numeric
     columns besides the target variable
-=======
-    Takes in train, validate, test and a list of features to scale
-    and scales those features.
-    Returns df with new columns with scaled data exluding target variable - LogError.
->>>>>>> 0681ff7d8f524d327020064a7b903e719d2340f7
     '''
     scale_features=list(train.select_dtypes(include=np.number).columns)
     scale_features.remove(target)
